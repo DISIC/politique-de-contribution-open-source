f28de26e0ecc5e14f733e971adbf769a85cfac2e
hmore <hugues.mo@gmail.com>
2017-12-06 18:21:28 +0100

    add pages

---
+++ _assets/scripts/main.js
@@ -0,0 +1,491 @@
+/*!
+anchor focus
+Fixes anchor focus in Chrome/Safari/IE
+*/
+// by setting the tabindex of the target container to -1 on click/enter
+// see -> http://stackoverflow.com/questions/3572843/skip-navigation-link-not-working-in-google-chrome/6188217#6188217
+
+$(document).ready(function(){
+
+  $("a[href^='#']").click(function(evt){
+    var anchortarget = $(this).attr("href");
+    $(anchortarget).attr("tabindex", -1).focus();
+  });
+
+  // Fixes anchor focus in Chrome/Safari/IE by setting the tabindex of the
+  // target container to -1 on page load
+  if (window.location.hash) {
+    $(window.location.hash).attr("tabindex", -1).focus();
+  }
+});
+
+jQuery(document).ready(function($) {
+
+    /*
+     * jQuery simple and accessible hide-show system (collapsible regions), using ARIA
+     * @version v1.7.0   
+     * Website: https://a11y.nicolas-hoffmann.net/hide-show/
+     * License MIT: https://github.com/nico3333fr/jquery-accessible-hide-show-aria/blob/master/LICENSE
+     */
+    // loading expand paragraphs
+    // these are recommended settings by a11y experts. You may update to fulfill your needs, but be sure of what you’re doing.
+    var attr_control = 'data-controls',
+        attr_expanded = 'aria-expanded',
+        attr_labelledby = 'data-labelledby',
+        attr_hidden = 'data-hidden',
+        $expandmore = $('.js-expandmore'),
+        $body = $('body'),
+        delay = 1500,
+        hash = window.location.hash.replace("#", ""),
+        multiexpandable = true,
+        expand_all_text = 'Tout déplier',
+        collapse_all_text = 'Tout replier';
+
+
+    if ($expandmore.length) { // if there are at least one :)
+        $expandmore.each(function(index_to_expand) {
+            var $this = $(this),
+                index_lisible = index_to_expand + 1,
+                options = $this.data(),
+                $hideshow_prefix_classes = typeof options.hideshowPrefixClass !== 'undefined' ? options.hideshowPrefixClass + '-' : '',
+                $to_expand = $this.next(".js-to_expand"),
+                $expandmore_text = $this.html();
+
+            $this.html('<button type="button" class="' + $hideshow_prefix_classes + 'expandmore__button js-expandmore-button">' + $expandmore_text + '</button>');
+            var $button = $this.children('.js-expandmore-button');
+
+            $to_expand.addClass($hideshow_prefix_classes + 'expandmore__to_expand').stop().delay(delay).queue(function() {
+                var $this = $(this);
+                if ($this.hasClass('js-first_load')) {
+                    $this.removeClass('js-first_load');
+                }
+            });
+
+            $button.attr('id', 'label_expand_' + index_lisible);
+            $button.attr(attr_control, 'expand_' + index_lisible);
+            $button.attr(attr_expanded, 'false');
+
+            $to_expand.attr('id', 'expand_' + index_lisible);
+            $to_expand.attr(attr_hidden, 'true');
+            $to_expand.attr(attr_labelledby, 'label_expand_' + index_lisible);
+
+            // quick tip to open (if it has class is-opened or if hash is in expand)
+            if ($to_expand.hasClass('is-opened') || (hash !== "" && $to_expand.find($("#" + hash)).length)) {
+                $button.addClass('is-opened').attr(attr_expanded, 'true');
+                $to_expand.removeClass('is-opened').removeAttr(attr_hidden);
+            }
+
+
+        });
+
+
+    }
+
+
+    $body.on('click', '.js-expandmore-button', function(event) {
+        var $this = $(this),
+            $destination = $('#' + $this.attr(attr_control));
+
+        if ($this.attr(attr_expanded) === 'false') {
+
+            if (multiexpandable === false) {
+                $('.js-expandmore-button').removeClass('is-opened').attr(attr_expanded, 'false');
+                $('.js-to_expand').attr(attr_hidden, 'true');
+            }
+
+            $this.addClass('is-opened').attr(attr_expanded, 'true');
+            $destination.removeAttr(attr_hidden);
+        } else {
+            $this.removeClass('is-opened').attr(attr_expanded, 'false');
+            $destination.attr(attr_hidden, 'true');
+        }
+
+        event.preventDefault();
+
+    });
+
+    $body.on('click keydown', '.js-expandmore', function(event) {
+        var $this = $(this),
+            $target = $(event.target),
+            $button_in = $this.find('.js-expandmore-button');
+
+        if (!$target.is($button_in) && !$target.closest($button_in).length) {
+
+            if (event.type === 'click') {
+                $button_in.trigger('click');
+                return false;
+            }
+            if (event.type === 'keydown' && (event.keyCode === 13 || event.keyCode === 32)) {
+                $button_in.trigger('click');
+                return false;
+            }
+
+        }
+
+
+    });
+
+    $body.on('click keydown', '.js-expandmore-all', function(event) {
+        var $this = $(this),
+            is_expanded = $this.attr('data-expand'),
+            $all_buttons = $('.js-expandmore-button'),
+            $all_destinations = $('.js-to_expand');
+
+        if (
+            event.type === 'click' ||
+            (event.type === 'keydown' && (event.keyCode === 13 || event.keyCode === 32))
+        ) {
+            if (is_expanded === 'true') {
+
+                $all_buttons.addClass('is-opened').attr(attr_expanded, 'true');
+                $all_destinations.removeAttr(attr_hidden);
+                $this.attr('data-expand', 'false').html(collapse_all_text);
+            } else {
+                $all_buttons.removeClass('is-opened').attr(attr_expanded, 'false');
+                $all_destinations.attr(attr_hidden, 'true');
+                $this.attr('data-expand', 'true').html(expand_all_text);
+            }
+
+        }
+
+
+    });
+
+
+});
+
+// menu-simple
+$(document).ready(function(){
+  $('#toggle-menu').click(function(e) {
+    e.preventDefault();
+    $('.nav-main-list').toggleClass('is-open');
+    $(this).toggleClass('is-open');
+  });
+});
+
+/*!
+Skip-link
+Show skip-link on focus
+*/
+
+$(document).ready(function(){
+  // affichage et masquage des liens d'évitement
+  $('.skip-link a').on('focus', function () {
+    $(this).parents('.container').addClass('skip-link-focus');
+  }).on('blur', function () {
+    $(this).parents('.container').removeClass('skip-link-focus');
+  });
+});
+
+/*!
+ * @copyright Copyright (c) 2017 IcoMoon.io
+ * @license   Licensed under MIT license
+ *            See https://github.com/Keyamoon/svgxuse
+ * @version   1.2.4
+ */
+/*jslint browser: true */
+/*global XDomainRequest, MutationObserver, window */
+(function () {
+    "use strict";
+    if (typeof window !== "undefined" && window.addEventListener) {
+        var cache = Object.create(null); // holds xhr objects to prevent multiple requests
+        var checkUseElems;
+        var tid; // timeout id
+        var debouncedCheck = function () {
+            clearTimeout(tid);
+            tid = setTimeout(checkUseElems, 100);
+        };
+        var unobserveChanges = function () {
+            return;
+        };
+        var observeChanges = function () {
+            var observer;
+            window.addEventListener("resize", debouncedCheck, false);
+            window.addEventListener("orientationchange", debouncedCheck, false);
+            if (window.MutationObserver) {
+                observer = new MutationObserver(debouncedCheck);
+                observer.observe(document.documentElement, {
+                    childList: true,
+                    subtree: true,
+                    attributes: true
+                });
+                unobserveChanges = function () {
+                    try {
+                        observer.disconnect();
+                        window.removeEventListener("resize", debouncedCheck, false);
+                        window.removeEventListener("orientationchange", debouncedCheck, false);
+                    } catch (ignore) {}
+                };
+            } else {
+                document.documentElement.addEventListener("DOMSubtreeModified", debouncedCheck, false);
+                unobserveChanges = function () {
+                    document.documentElement.removeEventListener("DOMSubtreeModified", debouncedCheck, false);
+                    window.removeEventListener("resize", debouncedCheck, false);
+                    window.removeEventListener("orientationchange", debouncedCheck, false);
+                };
+            }
+        };
+        var createRequest = function (url) {
+            // In IE 9, cross origin requests can only be sent using XDomainRequest.
+            // XDomainRequest would fail if CORS headers are not set.
+            // Therefore, XDomainRequest should only be used with cross origin requests.
+            function getOrigin(loc) {
+                var a;
+                if (loc.protocol !== undefined) {
+                    a = loc;
+                } else {
+                    a = document.createElement("a");
+                    a.href = loc;
+                }
+                return a.protocol.replace(/:/g, "") + a.host;
+            }
+            var Request;
+            var origin;
+            var origin2;
+            if (window.XMLHttpRequest) {
+                Request = new XMLHttpRequest();
+                origin = getOrigin(location);
+                origin2 = getOrigin(url);
+                if (Request.withCredentials === undefined && origin2 !== "" && origin2 !== origin) {
+                    Request = XDomainRequest || undefined;
+                } else {
+                    Request = XMLHttpRequest;
+                }
+            }
+            return Request;
+        };
+        var xlinkNS = "http://www.w3.org/1999/xlink";
+        checkUseElems = function () {
+            var base;
+            var bcr;
+            var fallback = ""; // optional fallback URL in case no base path to SVG file was given and no symbol definition was found.
+            var hash;
+            var href;
+            var i;
+            var inProgressCount = 0;
+            var isHidden;
+            var isXlink = false;
+            var Request;
+            var url;
+            var uses;
+            var xhr;
+            function observeIfDone() {
+                // If done with making changes, start watching for chagnes in DOM again
+                inProgressCount -= 1;
+                if (inProgressCount === 0) { // if all xhrs were resolved
+                    unobserveChanges(); // make sure to remove old handlers
+                    observeChanges(); // watch for changes to DOM
+                }
+            }
+            function attrUpdateFunc(spec) {
+                return function () {
+                    if (cache[spec.base] !== true) {
+                        if (spec.isXlink) {
+                            spec.useEl.setAttributeNS(xlinkNS, "xlink:href", "#" + spec.hash);
+                        } else {
+                            spec.useEl.setAttribute("href", "#" + spec.hash);
+                        }
+                    }
+                };
+            }
+            function onloadFunc(xhr) {
+                return function () {
+                    var body = document.body;
+                    var x = document.createElement("x");
+                    var svg;
+                    xhr.onload = null;
+                    x.innerHTML = xhr.responseText;
+                    svg = x.getElementsByTagName("svg")[0];
+                    if (svg) {
+                        svg.setAttribute("aria-hidden", "true");
+                        svg.style.position = "absolute";
+                        svg.style.width = 0;
+                        svg.style.height = 0;
+                        svg.style.overflow = "hidden";
+                        body.insertBefore(svg, body.firstChild);
+                    }
+                    observeIfDone();
+                };
+            }
+            function onErrorTimeout(xhr) {
+                return function () {
+                    xhr.onerror = null;
+                    xhr.ontimeout = null;
+                    observeIfDone();
+                };
+            }
+            unobserveChanges(); // stop watching for changes to DOM
+            // find all use elements
+            uses = document.getElementsByTagName("use");
+            for (i = 0; i < uses.length; i += 1) {
+                try {
+                    bcr = uses[i].getBoundingClientRect();
+                } catch (ignore) {
+                    // failed to get bounding rectangle of the use element
+                    bcr = false;
+                }
+                href = uses[i].getAttribute("href");
+                if (!href) {
+                    href = uses[i].getAttributeNS(xlinkNS, "href");
+                    isXlink = true;
+                } else {
+                    isXlink = false;
+                }
+                if (href && href.split) {
+                    url = href.split("#");
+                } else {
+                    url = ["", ""];
+                }
+                base = url[0];
+                hash = url[1];
+                isHidden = bcr && bcr.left === 0 && bcr.right === 0 && bcr.top === 0 && bcr.bottom === 0;
+                if (bcr && bcr.width === 0 && bcr.height === 0 && !isHidden) {
+                    // the use element is empty
+                    // if there is a reference to an external SVG, try to fetch it
+                    // use the optional fallback URL if there is no reference to an external SVG
+                    if (fallback && !base.length && hash && !document.getElementById(hash)) {
+                        base = fallback;
+                    }
+                    if (base.length) {
+                        // schedule updating xlink:href
+                        xhr = cache[base];
+                        if (xhr !== true) {
+                            // true signifies that prepending the SVG was not required
+                            setTimeout(attrUpdateFunc({
+                                useEl: uses[i],
+                                base: base,
+                                hash: hash,
+                                isXlink: isXlink
+                            }), 0);
+                        }
+                        if (xhr === undefined) {
+                            Request = createRequest(base);
+                            if (Request !== undefined) {
+                                xhr = new Request();
+                                cache[base] = xhr;
+                                xhr.onload = onloadFunc(xhr);
+                                xhr.onerror = onErrorTimeout(xhr);
+                                xhr.ontimeout = onErrorTimeout(xhr);
+                                xhr.open("GET", base);
+                                xhr.send();
+                                inProgressCount += 1;
+                            }
+                        }
+                    }
+                } else {
+                    if (!isHidden) {
+                        if (cache[base] === undefined) {
+                            // remember this URL if the use element was not empty and no request was sent
+                            cache[base] = true;
+                        } else if (cache[base].onload) {
+                            // if it turns out that prepending the SVG is not necessary,
+                            // abort the in-progress xhr.
+                            cache[base].abort();
+                            delete cache[base].onload;
+                            cache[base] = true;
+                        }
+                    } else if (base.length && cache[base]) {
+                        setTimeout(attrUpdateFunc({
+                            useEl: uses[i],
+                            base: base,
+                            hash: hash,
+                            isXlink: isXlink
+                        }), 0);
+                    }
+                }
+            }
+            uses = "";
+            inProgressCount += 1;
+            observeIfDone();
+        };
+        var winLoad;
+        winLoad = function () {
+            window.removeEventListener("load", winLoad, false); // to prevent memory leaks
+            tid = setTimeout(checkUseElems, 0);
+        };
+        if (document.readyState !== "complete") {
+            // The load event fires when all resources have finished loading, which allows detecting whether SVG use elements are empty.
+            window.addEventListener("load", winLoad, false);
+        } else {
+            // No need to add a listener if the document is already loaded, initialize immediately.
+            winLoad();
+        }
+    }
+}());
+
+/*!
+  Scampi textarea-counter
+  Décompte les caractères restants dans les éléments textarea disposant d’un maxlength
+  Ajoute des attributs aria pour annoncer le nombre de caractères restant
+*/
+
+function textareaCounter() {
+  var $textAreaField = $("textarea[maxlength]");
+  var stepPolite     = 100;
+  var stepAssertive  = 20;
+
+  $textAreaField.each(function (index, textarea) {
+
+    var maxLength      = $(textarea).attr("maxlength");
+    var messageLength  = $(textarea).val().length;
+    var activeValue    = countRest(maxLength, messageLength);
+    var $textarea      = undefined;
+    var $paragraph     = undefined;
+
+    $(textarea).after('<p class="textarea-counter"><span class="textarea-counter-nb">' + activeValue +'</span> caractères restants</p>');
+
+    $(textarea).on("input", function (e) {
+      $textarea = $(e.currentTarget);
+      $paragraph = $textarea.next('p.textarea-counter');
+      updateValue($textarea, $paragraph);
+    });
+
+  });
+
+  function countRest(maxlength, messageLength) {
+    return maxlength - messageLength;
+  }
+
+  function countStepPolite(maxLengthValue) {
+    return maxLengthValue - stepPolite;
+  }
+
+  function countStepAssertive(maxLengthValue) {
+    return maxLengthValue - stepAssertive;
+  }
+
+  function updateAria(maxLengthValue, messageLength, $paragraph) {
+    politeFlag    = countStepPolite(maxLengthValue);
+    assertiveFlag = countStepAssertive(maxLengthValue);
+
+    if (messageLength < politeFlag) {
+      $paragraph
+        .removeAttr('aria-live')
+        .removeAttr('aria-atomic');
+    }
+    else if (messageLength >= politeFlag && messageLength < assertiveFlag) {
+      $paragraph.attr({
+        'aria-live': 'polite',
+        'aria-atomic': 'true'
+      });
+    }
+    else if (messageLength >= assertiveFlag) {
+      $paragraph.attr({
+        'aria-live': 'assertive',
+        'aria-atomic': 'true'
+      });
+    }
+  }
+
+  function updateValue($textarea, $paragraph) {
+    var maxLength = $textarea.attr("maxlength");
+    var messageLength = $textarea.val().length;
+    $paragraph.find('.textarea-counter-nb').text(countRest(maxLength, messageLength));
+    updateAria(maxLength, messageLength, $paragraph);
+  }
+
+}
+
+$(document).ready(function(){
+  textareaCounter();
+});
